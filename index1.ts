48bbdb02d13b7bb63de4857e4f4cde38e1d3cf4b
Jinxing Lin <172601673@qq.com>
2019-07-08 12:02:04 +0800

    Remove test files

--- index1.ts
+++
@@ -1,9 +0,0 @@
-import Crc32 from './lib/png/crc32';
-import { format } from './lib/colorUtils';
-import PngPalette from './lib/png/pngPalette';
-
-const a1 = new PngPalette([]);
-
-const a = new Crc32();
-
-console.log(a, format(0xFFFFFF));
