e87182550c09a301574a85b2c51babe8ebbd0c2b
dmester <daniel@mester.se>
2019-09-14 16:14:25 +0200

    Updated JSDoc.

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ module.exports = {
     /**
      * Creates a new canvas.
      * @param {number} width  Width of the canvas in pixels.
-     * @param {number} heigt  Height of the canvas in pixels.
+     * @param {number} heigth  Height of the canvas in pixels.
      * @returns {Canvas}
      */
     createCanvas: function (width, height) {
